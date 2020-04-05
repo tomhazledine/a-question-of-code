6d02c717cbc4383325121b2a9c7fd6e74eceaf84
tomhazledine <tom@tomhazledine.com>
2020-04-05 17:28:10 +0100

    mounting React as Player on episode page

---
+++ src/js/components/Player.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Player = ({ url }) => {
+    console.log("url", url);
+    return <h1>Player</h1>;
+};
+
+export default Player;
--- src/js/player.js
+++ src/js/player.js
@@ -1,1 +1,12 @@
-console.log("player");
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Player from "./components/Player";
+
+const mountPoint = document.getElementById("player__wrapper");
+
+if (mountPoint) {
+    const audioPath = mountPoint.getAttribute("data-audio");
+
+    ReactDOM.render(<Player url={audioPath} />, mountPoint);
+}
